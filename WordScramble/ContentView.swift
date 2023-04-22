ba546e4cd8a2d3ecdd3cd90dcf7a17d0fc89b93e
Vefa Kosova <vefa417@icloud.com>
2023-04-22 23:30:29 +0300

    .

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,15 +8,167 @@
 import SwiftUI
 
 struct ContentView: View {
+//1    let people = ["Finn", "Leia", "Luke", "Rey"]
+    
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        //1        List {
+        //            Section("Section 1") {
+        //                Text("Static row 1")
+        //                Text("Static row 2")
+        //            }
+        //            Section("Section 2") {
+        //                ForEach(0..<5) {
+        //                    Text("Dynamic row \($0)")
+        //                }
+        //            }
+        //            Section("Section 3") {
+        //                Text("Static row 3")
+        //                Text("Static row 4")
+        //            }
+        //        }
+        //        .listStyle(.grouped)
+        //1        List(0..<5) {
+        //            Text("Dynamic row \($0)")
+        //        }
+        //1        List(people, id: \.self) {
+        //            Text($0)
+        //        }
+        
+        
+        NavigationView {
+            List {
+                Section {
+                    TextField("Enter your word", text: $newWord)
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                }
+                
+                Section {
+                    ForEach(usedWords, id: \.self) { word in
+                        HStack {
+                            Image(systemName: "\(word.count).circle")
+                            Text(word)
+                        }
+                    }
+                }
+            }
+            .navigationTitle(rootWord)
+            .onSubmit(addNewWord)
+            .onAppear(perform: startGame)
+            .alert(errorTitle, isPresented: $showingError) {
+                Button("OK", role: .cancel) { }
+            } message: {
+                Text(errorMessage)
+            }
         }
-        .padding()
+        
     }
+    
+    func addNewWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        guard answer.count > 0 else { return }
+        
+        guard isOriginal(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original!")
+            return
+        }
+        guard isPossible(word: answer) else {
+            wordError(title: "Word not possible", message: "You can't spell that word from '\(rootWord)'!")
+            return
+        }
+        guard isReal(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+        
+        withAnimation {
+            usedWords.insert(answer, at: 0)
+        }
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                return
+            }
+        }
+        
+        fatalError("Could not load start.txt from bundle.")
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool {
+        var tempWord = rootWord
+        
+        for letter in word {
+            if let pos = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: pos)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+//2    func loadFile() {
+//        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+//            if let fileContents = try? String(contentsOf: fileURL) {
+//                fileContents
+//            }
+//        }
+//    }
+    
+    
+//3    func test() {
+//        let input = """
+//a
+//b
+//c
+//"""
+//        let letters = input.components(separatedBy: "\n")
+//        //-letters returning array
+//        let letter = letters.randomElement()
+//        //-letter optional array can be empty
+//        let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+//3        let word = "swift"
+//        let checker = UITextChecker()
+//
+//        let range = NSRange(location: 0, length: word.utf16.count)
+//        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+//
+//        let allGood = misspelledRange.location == NSNotFound
+//    }
+        
 }
 
 struct ContentView_Previews: PreviewProvider {
